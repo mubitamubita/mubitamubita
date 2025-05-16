ac2881cb924dacf1a0a5577b8223a8736f324357
* <josephmubita@gmail.com>
2025-05-17 00:29:49 +0200

    add space

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 python manage.py migrate
 echo "from django.contrib.auth import get_user_model; User = get_user_model();
-User.objects.filter(username='admin').exists() or
-User.objects.create_superuser('admin', 'admin@example.com', 'password123')" |
+User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'password123')" |
 python manage.py shell
