51dc4a816421d72951a8d5c41723265f17583e92
* <josephmubita@gmail.com>
2025-05-17 00:42:50 +0200

    add collectstatis

--- render-build.sh
+++ render-build.sh
@@ -3,3 +3,5 @@ python manage.py migrate
 echo "from django.contrib.auth import get_user_model; User = get_user_model();
 User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'password123')" |
 python manage.py shell
+
+python manage.py collectstatic --noinput
