a2ad230261b5b7cc5513d7f17f9510dcc7541b73
* <josephmubita@gmail.com>
2025-05-17 05:50:02 +0200

    add database url

--- mubitajmubita/settings.py
+++ mubitajmubita/settings.py
@@ -86,11 +86,10 @@ WSGI_APPLICATION = 'mubitajmubita.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  
+  'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
 
 if os.environ.get('DATABASE_URL'):
   DATABASES['default'] = dj_database_url.config(
